8430ee65190a3703f0f2026b57b4c92f2a5ef050
karam <makarmelnyk@gmail.com>
2024-10-10 21:59:29 +0200

    updates

--- src/main/java/fr/heriamc/bukkit/game/packet/GameCreationRequestPacket.java
+++ src/main/java/fr/heriamc/bukkit/game/packet/GameCreationRequestPacket.java
@@ -2,6 +2,7 @@ package fr.heriamc.bukkit.game.packet;
 
 import fr.heriamc.api.messaging.packet.HeriaPacket;
 import fr.heriamc.api.messaging.packet.HeriaPacketChannel;
+import fr.heriamc.api.server.HeriaServerType;
 
 import java.util.UUID;
 
@@ -9,11 +10,13 @@ public class GameCreationRequestPacket extends HeriaPacket {
 
     private final UUID requestID;
     private final String server;
+    private final HeriaServerType serverType;
 
-    public GameCreationRequestPacket(UUID requestID, String server) {
+    public GameCreationRequestPacket(UUID requestID, String server, HeriaServerType serverType) {
         super(HeriaPacketChannel.GAME);
         this.requestID = requestID;
         this.server = server;
+        this.serverType = serverType;
     }
 
     public UUID getRequestID() {
@@ -22,5 +25,9 @@ public class GameCreationRequestPacket extends HeriaPacket {
 
     public String getServer() {
         return server;
+    }
+
+    public HeriaServerType getServerType() {
+        return serverType;
     }
 }
